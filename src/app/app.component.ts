a868ead96a4361436c2f00498a4e034a136be254
Seyha Vorn <vornseyha4758@gmail.com>
2023-05-21 18:37:45 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'intro';
-  pro: boolean = true;
-
-  constructor(
-      private http: HttpClient
-  ) {
-  }
+export class AppComponent {
+  title = 'introduction';
 
   ngOnInit() {
-    setTimeout(() => {
-      this.pro = false;
-    }, 1000);
   }
 }
